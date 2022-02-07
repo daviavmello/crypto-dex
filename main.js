1bb9f28cab0612cd14fced2d09c6ea59b7376800
Davi Mello <davi.mello@orion.com>
2022-02-07 17:51:11 -0500

    select list

--- main.js
+++ main.js
@@ -2,10 +2,28 @@ Moralis.initialize("8mhQ8vxPNUaWL5v44ygPoWn0ovTJ1muKCGzL0XtB");
 Moralis.serverURL = "https://vojnqi99exj1.usemoralis.com:2053/server";
 
 async function init() {
-  const tokens = await Moralis.Plugins.oneInch.getSupportedTokens({
-    chain: "bsc",
+  await Moralis.initPlugins();
+  await Moralis.enable();
+  await listAvailableTokens();
+}
+
+async function listAvailableTokens() {
+  const result = await Moralis.Plugins.oneInch.getSupportedTokens({
+    chain: "eth",
   });
-  console.log(tokens);
+  const tokens = result.tokens;
+  let parent = document.getElementById("token-list");
+  for (const address in tokens) {
+    let token = tokens[address];
+    let div = document.createElement("div");
+    div.classList.add("row");
+    let html = `
+      <img class="token-list-img" src="${token.logoURI}">
+      <span class="token-list-text>${token.symbol}</span>
+    `;
+    div.innerHTML = html;
+    parent.appendChild(div);
+  }
 }
 
 async function login() {
@@ -26,6 +44,8 @@ const openModal = () => {
 const closeModal = () => {
   document.getElementById("token-modal").style.display = "none";
 };
+
+init();
 
 document.getElementById("from-token-select").onclick = openModal;
 document.getElementById("modal-close").onclick = closeModal;
