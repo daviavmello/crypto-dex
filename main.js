cfcb548cde3b0ec5451766f0ba5f0d7ab48f9447
Davi Mello <davi.mello@orion.com>
2022-02-07 19:45:14 -0500

    data address attributye

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 Moralis.initialize("8mhQ8vxPNUaWL5v44ygPoWn0ovTJ1muKCGzL0XtB");
 Moralis.serverURL = "https://vojnqi99exj1.usemoralis.com:2053/server";
 
+let currentTrade = {};
+
 async function init() {
   await Moralis.initPlugins();
   await Moralis.enable();
@@ -16,14 +18,22 @@ async function listAvailableTokens() {
   for (const address in tokens) {
     let token = tokens[address];
     let div = document.createElement("div");
-    div.classList.add("row");
+    div.setAttribute("data-address", "address");
+    div.classList.add("token-row");
     let html = `
       <img class="token-list-img" src="${token.logoURI}">
       <span class="token-list-text>${token.symbol}</span>
     `;
     div.innerHTML = html;
+    div.onclick = selectToken();
     parent.appendChild(div);
   }
+}
+
+async function selectToken() {
+  closeModal();
+  let address = event.target.getAttribute("data-address");
+  console.log(address);
 }
 
 async function login() {
